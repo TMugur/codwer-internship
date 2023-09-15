5c293d0f765fcbbe23d626596ee527ace76c7d8c
Tudor Mugureanu <tudormug@gmail.com>
2023-09-15 18:27:01 +0300

    1

--- basicapp1/Program.cs
+++ basicapp1/Program.cs
@@ -6,5 +6,35 @@ using System.Threading.Tasks;
 
 namespace basicapp1
 {
-   
+   class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number with least 3 digits: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            if (number < 100)
+            {
+                Console.WriteLine("The number have less than 3 digits. Enter another number.");
+                input = Console.ReadLine();
+                number = int.Parse(input);
+            }
+            int reversNumber = 0;
+            while (number > 0)
+            {
+                int num = number % 10;
+                reversNumber = reversNumber *10 + num;
+                number /= 10;
+            }
+            int square = (int)Math.Sqrt(reversNumber);
+            if (square * square ==reversNumber)
+            {
+                Console.WriteLine("This number is perfect square.");
+            }
+            else
+            {
+                Console.WriteLine("This number is not perfect square.");
+            }
+        }
+    }
 }
